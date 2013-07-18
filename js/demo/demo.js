80d79ac9b4bd605375df61e8446942e1772a2132
Silver Spoon <silverspoon1120.sw@gmail.com>
2013-07-18 12:23:03 -0700

    formatting (nesting), consolidation

--- js/demo/demo.js
+++ js/demo/demo.js
@@ -4,24 +4,10 @@ var EffecktDemos = {
 
     $(window).load(function() {
       $(".no-transitions").removeClass("no-transitions");
-      EffecktDemos.transitionEndEventName = EffecktDemos.transitionEndEventNames[Modernizr.prefixed('transition')];
-      EffecktDemos.animationEndEventName = EffecktDemos.animationEndEventNames[Modernizr.prefixed('animation')];
     });
-  },
 
-  animationEndEventNames: {
-    'WebkitAnimation' : 'webkitAnimationEnd',
-    'OAnimation' : 'oAnimationEnd',
-    'msAnimation' : 'MSAnimationEnd',
-    'animation' : 'animationend'
-  },
 
-  transitionEndEventNames: {
-    'WebkitTransition' : 'webkitTransitionEnd',
-    'OTransition' : 'oTransitionEnd',
-    'msTransition' : 'MSTransitionEnd',
-    'transition' : 'transitionend'
-  },
+  }
 
 }
 
